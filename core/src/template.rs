46eedbf7d07a98079e368386c6e0d66d405492c5
Theryston <funktodo2@gmail.com>
2022-07-06 13:33:10 -0300

    ci: format files

--- core/src/template.rs
+++ core/src/template.rs
@@ -1,34 +1,34 @@
 pub struct TemplateHandlerOptions {
-    pub name: String,
+  pub name: String,
 }
 
 pub struct TemplateHandler {
-    pub name: String,
+  pub name: String,
 }
 
 impl TemplateHandler {
-    pub fn new(options: TemplateHandlerOptions) -> Self {
-        let template_handler = Self {
-            name: options.name.clone(),
-        };
-        template_handler
-    }
+  pub fn new(options: TemplateHandlerOptions) -> Self {
+    let template_handler = Self {
+      name: options.name.clone(),
+    };
+    template_handler
+  }
 
-    pub fn get_template_by_name(&self, name: String) -> String {
-        String::from(name)
-    }
+  pub fn get_template_by_name(&self, name: String) -> String {
+    String::from(name)
+  }
 }
 
 #[cfg(test)]
 pub mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn get_template_by_name() {
-        let template_handler = TemplateHandler::new(TemplateHandlerOptions {
-            name: String::from("test"),
-        });
-        let template = template_handler.get_template_by_name(String::from("test"));
-        assert_eq!(template, String::from("test"));
-    }
+  #[test]
+  fn get_template_by_name() {
+    let template_handler = TemplateHandler::new(TemplateHandlerOptions {
+      name: String::from("test"),
+    });
+    let template = template_handler.get_template_by_name(String::from("test"));
+    assert_eq!(template, String::from("test"));
+  }
 }
